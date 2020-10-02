0448917a845ac5ce92aca6f81e14d09e56e2d8b0
Vinicius Nakamura <hello@vnakamura.com>
2020-10-02 17:33:11 -0300

    Add farmblocks Card

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
+import { Flex, Box } from 'reflexbox';
 import logo from './logo.svg';
 import './App.css';
+
+import Card from '@crave/farmblocks-card';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Flex>
+          <img src={logo} className="App-logo" alt="logo" />
+          <Box>
+            <Card css="color: red">Hello!!</Card>
+            <p>
+              Edit <code>src/App.jsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </Box>
+        </Flex>
       </header>
     </div>
   );
